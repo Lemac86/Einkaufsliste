fb67f004695b8c1d51d1851cba9e8d54f775c030
Lemac86 <T0bsen86@web.de>
2023-07-10 14:23:19 +0000

    noch alles heil, vor der Swipe Component

--- resources/js/Components/ArticleButton.vue
+++ resources/js/Components/ArticleButton.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="flex">
-        <HakenIcon v-if="buttonColor == 'bg-green-600'" class="block h-9 w-auto fill-current text-gray-800"></HakenIcon>
-        <button @click="changeColor"
-            class="inline-flex items-center px-4 py-1 border border-transparent rounded-md font-semibold text-lg text-white tracking-widest transition ease-in-out duration-150 me-2"
-            :class="`${buttonColor}`">
+        <HakenIcon v-if="checked" class="block h-9 w-auto fill-current text-gray-800"></HakenIcon>
+        <button @click="checked = !checked"
+            class="inline-flex items-center px-4 py-1 border border-transparent rounded-md font-semibold text-lg text-white tracking-widest transition ease-in-out duration-50 me-2"
+            :class="checked ? 'bg-green-600' : 'bg-blue-600'">
             <slot />
         </button>
     </div>
@@ -12,9 +12,8 @@
 <script setup>
 import { ref } from "vue";
 import HakenIcon from '@/Components/HakenIcon.vue';
+import { computed } from "vue";
 
-const buttonColor = ref('bg-blue-600')
-function changeColor() {
-    buttonColor.value == "bg-blue-600" ? buttonColor.value = 'bg-green-600' : buttonColor.value = "bg-blue-600"
-}
+const checked = ref(false)
+
 </script>
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -13,7 +13,7 @@ const showingNavigationDropdown = ref(false);
 <template>
     <div>
         <div class="min-h-screen bg-blue-200">
-            <nav class="bg-white border-b border-gray-100">
+            <nav class="bg-blue-300 border-b border-gray-100">
                 <!-- Primary Navigation Menu -->
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div class="flex justify-between h-16">
@@ -26,7 +26,7 @@ const showingNavigationDropdown = ref(false);
                             </div>
 
                             <!-- Navigation Links -->
-                            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex text-white-100">
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </NavLink>
@@ -91,7 +91,7 @@ const showingNavigationDropdown = ref(false);
 
                 <!-- Responsive Navigation Menu -->
                 <div :class="{ block: showingNavigationDropdown, hidden: !showingNavigationDropdown }" class="sm:hidden">
-                    <div class="pt-2 pb-3 space-y-1">
+                    <div class="pt-2 pb-3 space-y-1 text-white">
                         <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
                             Dashboard
                         </ResponsiveNavLink>
